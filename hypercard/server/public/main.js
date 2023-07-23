14b95788725ab0b2dbcb56958279ec353ab8da4d
Mykyta Storozhenko <mst387@uky.edu>
2023-07-23 16:36:12 -0400

    HyperCard 2.0 Up and Running

--- hypercard/server/public/main.js
+++ hypercard/server/public/main.js
@@ -1,3 +1,5 @@
+let targetElement = null; // Variable to store the clicked element
+
 // Function to handle the command
 function handleCommand() {
     let command = document.getElementById('command');
@@ -43,5 +45,75 @@ document.getElementById('command').addEventListener('keydown', function(event) {
 
 // Event listener for clicks in the card div
 document.getElementById('card').addEventListener('click', function(event) {
-    document.getElementById('command').value = 'Regarding ' + event.target.tagName.toLowerCase() + ': ';
+    document.getElementById('command').value = 'Regarding ' + event.target.id + ': ';
 });
+
+// Event listener for clicks on any child of the card div
+document.getElementById('card').addEventListener('click', function(event) {
+    if (event.target.id !== 'card') { // Ignore clicks on the card itself
+        targetElement = event.target; // Store the clicked element
+        let modal = document.getElementById('modal');
+        modal.style.display = 'block'; // Show the modal
+        document.getElementById('modal-text').value = targetElement.textContent; // Populate the text input with the current text
+    }
+});
+
+// Event listener for the Save button in the modal
+document.getElementById('modal-save').addEventListener('click', function() {
+    if (targetElement) { // If an element was clicked
+        targetElement.textContent = document.getElementById('modal-text').value; // Update the text
+        targetElement.style.color = document.getElementById('modal-color').value; // Update the color
+    }
+    document.getElementById('modal').style.display = 'none'; // Hide the modal
+});
+
+
+//publish to server 
+document.getElementById('publish').addEventListener('click', function() {
+    const title = prompt("Enter a title for your card: ");
+    const author = prompt("Enter your name: ");
+    let cardHtml = document.getElementById('card').outerHTML;
+    let blob = new Blob([cardHtml], {type: 'text/html'});
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement('a');
+    link.href = url;
+    link.download = `${title}_${author}.html`;
+    link.click();
+    //publish to server
+    fetch('/publish', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            html: cardHtml,
+            title: title,
+            author: author
+        })
+    })
+    .then(response => response.text())
+    .then(text => {
+        // do something with the response...
+        console.log(text);
+    })
+    .catch(error => console.error(error));
+}
+);
+
+// Event listener for the browse button to go to cards home html
+document.getElementById('browse').addEventListener('click', function() {
+    window.location.href = './cards/home.html';
+}
+);
+
+//event listener for div clear
+document.getElementById('clear').addEventListener('click', function() {
+    document.getElementById('inner-card').innerHTML = '';
+}
+);
+
+//modal cancel 
+document.getElementById('modal-cancel').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+}
+);
--- hypercard/server/server.js
+++ hypercard/server/server.js
@@ -6,7 +6,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const { type } = require('os');
 
 const configuration = new Configuration({
-  apiKey:"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",
+  /*API Key*/
 });
 const openai = new OpenAIApi(configuration);
 
@@ -42,7 +42,15 @@ async function getCompletion(div, command) {
     try {
       const completion = await openai.createChatCompletion({
         model: 'gpt-4',
-        messages: [{ role: 'user', content: `You are hyperCardGPT and your tast is to return code and only code that will be injected into an html div tagged 'card'. You will receive the div as it currently is, and a command as to what to do. Your task it to just return the inner html implementing the command, and nothing else. Here is the inner html of the div now: ${div} and here is the command: ${command}` }],
+        temperature: 0.5,
+        messages: [{ role: 'user', content: `
+You are HyperCardGPT, an AI designed to generate HTML code that is injected into a specific HTML structure. Your task involves creating content that will fit within an inner div, tagged as 'inner-card', nested inside an outer div, tagged as 'card'. Always ensure that the content is returned within the tags <div id="inner-card"></div>.
+
+Remember, under no circumstances should you alter the height, width, or any other properties of the inner div that might affect the outer div. Maintain the dimensions of the inner div and make it vertically scrollable if needed, ensuring it is always fully enclosed by the outer div.
+
+You will receive the current state of the div and a command to implement. Respond with the updated inner HTML that fulfills the command—no explanations, no markdown, just pure HTML and inline CSS. Make certain that any element you add has a unique tag, avoiding repetition.
+
+Here's your current div: ${div} and your command: ${command}. Follow all instructions diligently.` }],
       });
   
       console.log(completion.data.choices[0].message.content);
@@ -52,4 +60,41 @@ async function getCompletion(div, command) {
     }
   };
 
+app.post('/publish', cors(), (req, res) => {
+  const data = req.body;
+  const html = data.html;
+  const title = data.title;
+  const author = data.author;
+  const filename = `./public/cards/${title}_${author}.html`;
+  fs.writeFile(filename, html, function(err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error writing file');
+    } else {
+      res.send('File saved');
+    }
+  });
+});
 
+app.get('/cards', cors(), (req, res) => {
+  const cards = [];
+  fs.readdir('./public/cards', (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading directory');
+    } else {
+      files.forEach(file => {
+        const parts = file.split('_');
+        if (parts.length === 2) {
+          const card = {
+            title: parts[0],
+            author: parts[1].split('.')[0],
+            link: `/cards/${file}`
+          };
+          cards.push(card);
+        }
+      });
+      res.send(cards);
+    }
+  });
+});
